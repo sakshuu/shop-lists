2f31c95d631214ce8eea409f31503b57cc9a5c22
sakshi jadhav <sakshisjadhav.120@gmail.com>
2023-11-25 14:36:08 +0530

    updated

--- frontend/src/components/index.js
+++
@@ -1,1 +0,0 @@
-export { default as MyDropdown } from "./MyDropdown" 
--- frontend/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home" 
-export { default as MyModal } from "./MyModal" 
-export { default as ShopListForm } from "./ShopListForm" 
-export { default as PageNotFound } from "./PageNotFound" 
